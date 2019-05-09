d3b0223c8ea4e0db11cafb41bff4732a56c8b3b7
Mehmet KANBAZ <mkanbaz@outlook.com>
2019-05-09 12:34:25 +0300

    +

---
+++ 1. Hafta/1. Hafta/Controllers/AnaController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1.Hafta.Controllers
+{
+    public class AnaController : Controller
+    {
+        // GET: Ana
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+
+        public ActionResult VieweVeriGonderme()
+        {
+            string viewbag = "ViewBag ile veri gönderme";
+            string viewdata = "ViewData ile veri gönderme";
+            string tempdata = "TempData ile veri gönderme";
+
+            ViewBag.Viewbagornek = viewbag;
+            ViewData["Viewdataornek"] = viewdata;
+            TempData["Tempdataornek"] = tempdata;
+            return View();
+        }
+
+
+        public ActionResult HtmlHelperKullanimi()
+        {
+            return View();
+        }
+
+
+        public ActionResult ViewdenControlleraVeriGonderme()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ViewdenControlleraVeriGonderme(string kullaniciadi, string sifre, string cinsiyet, bool? sozlesme, string adres, string Sehir)
+        {
+            ViewBag.Kullanici = kullaniciadi;
+            if (sozlesme == true)
+            {
+                
+            }
+            else
+            {
+                ViewBag.Durum = "Sözleşme Kabul edilmedi";
+            }
+            return View();
+        }
+    }
+}
